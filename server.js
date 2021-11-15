4a9ee7aed3c2e19c14a34811a0fb0b75fb2f9ad3
Jay Reische <ReischeJay@JohnDeere.com>
2021-11-15 09:14:29 -0600

    working on backend

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 const express = require("express");
+require('dotenv').config();
 const app = express(); //app is an object
 //include the method-override package
 const methodOverride = require("method-override");
 app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-// const routes = require('./routes');
+const routes = require('./routes');
 const port = 3000;
 const cors = require('cors');
+
+const corsOptions = {
+    origin: ['http://localhost:3000'],
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true, //allows session cookies to be sent back and forth
+    optionsSuccessStatus: 200 //legacy browsers
+  }
 
 //middleware
 app.use(cors());
 app.use(express.json());
-app.get('/', (req, res) => {
-    res.redirect('/users')
-})
-  
+app.use('/user', routes.user);
+app.use('/movie', routes.movie);
 
 app.listen(3000, () => {
     console.log(`Server running on port: ${port}`);
