36863e109db26c11ed2ae17795c351d4399b2c02
Jay Reische <ReischeJay@JohnDeere.com>
2021-11-18 10:41:41 -0600

    adjusting backend

--- controllers/movie.js
+++ controllers/movie.js
@@ -1,56 +1,57 @@
-const User = require('../models').User;
-const Movie = require('../models').Movie;
+const User = require("../models").User;
+const Movie = require("../models").Movie;
 
 // Adding a movie to the database
 const addMovie = (req, res) => {
-    Movie.create(req.body)
-    .then(newMovie => {
-        res.json(newMovie);
-    })
-}
+  Movie.create(req.body).then((newMovie) => {
+    res.json(newMovie);
+  });
+};
 
 // Used to show a movie
 const showMovie = (req, res) => {
-    Movie.findByPk(req.params.index, {
-      include: [
-        {
-          model: User,
-          attributes: ["username", "id"],
-        },
-      ],
-    }).then((foundMovie) => {
-      console.log("found Movie", foundMovie.name);
-      res.json(foundMovie);
-    });
-  };
+  Movie.findByPk(req.params.index, {
+    include: [
+      {
+        model: User,
+        attributes: ["username", "id"],
+      },
+    ],
+  }).then((foundMovie) => {
+    console.log("found Movie", foundMovie.name);
+    res.json(foundMovie);
+  });
+};
 
 // One item will be deleted
 const deleteMovie = (req, res) => {
-    Movie.destroy({
-      where: { id: req.params.id },
-    }).then(() => {
-      res.redirect("/songs");
-    });
-  };
+  Movie.destroy({
+    where: { id: req.params.id },
+  }).then(() => {
+    res.redirect("/songs");
+  });
+};
 
 const getAllMovies = (req, res) => {
-    console.log(Movie);
-    Movie.findAll({
-        include: [
-            {
-                model: User,
-                attributes: ["username", "id"],
-            },
-        ],
-    })
-        .then((movie) => {
-            res.json(movie);
-        })
-}
+  console.log(Movie);
+  Movie.findAll({
+    where: {
+      user_id: req.params.index,
+    },
+    include: [
+      {
+        model: User,
+        attributes: ["username", "id"],
+      },
+    ],
+  }).then((movie) => {
+    res.json(movie);
+  });
+};
 
 module.exports = {
-    addMovie,
-    showMovie,
-    deleteMovie,
-    getAllMovies
-}
+  addMovie,
+  showMovie,
+  deleteMovie,
+  getAllMovies,
+};
--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ const ctrl = require("../controllers");
 
 router.post("/signup", ctrl.users.signup);
 router.get("/profile/:index", ctrl.users.renderProfile);
-
+router.get("/profile/:index/movies"), ctrl.movies.getAllMovies;
 router.post("/login", ctrl.users.login);
 router.put("/profile/:index", ctrl.users.editProfile);
 
